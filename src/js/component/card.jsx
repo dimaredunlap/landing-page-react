049cc8d3d7c2076e636f878d1b45c4dc483a7dbf
dimaredunlap <dimaredunlap@gmail.com>
2022-07-26 19:06:37 +0000

    needs styling

--- src/js/component/card.jsx
+++ src/js/component/card.jsx
@@ -12,7 +12,7 @@ const Card = (props) => {
          		<div className="card-body">
             	<h5 className="card-title">{props.title}</h5>
             	<p className="card-text">{props.description}</p>
-            	<a href="#" className="btn btn-primary">Go somewhere</a>
+            	<a href="#" className="btn btn-primary">Find Out More!</a>
           	</div>
         </div>
 	);
